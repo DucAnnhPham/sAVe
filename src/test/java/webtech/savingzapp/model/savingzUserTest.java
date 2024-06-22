9d066f7ebd1e3bdc311f4961fff34f75e4bde04a
ducanh <d-pham@outlook.de>
2024-06-22 20:18:33 +0200

    added tests

---
+++ src/test/java/webtech/savingzapp/model/savingzUserTest.java
@@ -0,0 +1,30 @@
+package webtech.savingzapp.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class savingzUserTest {
+
+    @Test
+    void testToString() {
+        //Eingabedaten
+        String username = "Test";
+        String email = "Test";
+        String password = "pw";
+
+        //System under Test aufsetzen
+        Savingz_User user = new Savingz_User(username,email,password);
+        user.setId(10L);
+
+        //erwartetets Ergebnis
+        String expected = "Savingz_User{id=10, username='Test', email='Test', password='pw'}";
+
+        //tatächliches Ergebnis
+        String actual = user.toString();
+
+        //Vergleich
+        assertEquals(expected,actual);
+    }
+
+  
+}
